ddc92502bb880b0166e457494b306a61c43ea2fe
CynthiaMugo <cynthiamugo25@gmail.com>
2022-09-02 04:58:48 -0700

    memory blocks

--- aux_mem.c
+++ aux_mem.c
@@ -26,7 +26,7 @@ void _memcpy(void *newptr, const void *ptr, unsigned int size)
  * if new_size == old_size, returns ptr without changes
  * if malloc fails, returns NULL
  */
-void *realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	void *newptr;
 
